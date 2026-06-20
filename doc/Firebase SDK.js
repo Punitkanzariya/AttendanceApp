0889731601d6ae0bb3c5b8568de46d6c9fbea7fe
darshanTechsture <darshanm.techsture@gmail.com>
2026-06-20 14:03:48 +0530

    firebase-sdk added

---
+++ doc/Firebase SDK.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAjB_V7X1MLxZF8r8h7dW8ITaTRBlzBFtA",
+    authDomain: "techsture-hrm.firebaseapp.com",
+    projectId: "techsture-hrm",
+    storageBucket: "techsture-hrm.firebasestorage.app",
+    messagingSenderId: "151284434763",
+    appId: "1:151284434763:web:cf2c700500ae68a2fbcace"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
